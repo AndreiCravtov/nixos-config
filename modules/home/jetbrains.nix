f38d55acbdd4e9656f3d35116207254ed8ecd9bc
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2026-01-01 22:51:48 +0000

    updated flake

--- modules/home/jetbrains.nix
+++ modules/home/jetbrains.nix
@@ -5,13 +5,15 @@
 }: let
   inherit (flake.inputs) nix-jetbrains-plugins;
   inherit (pkgs.stdenv.hostPlatform) system;
+
+  mkJetbrains = nix-jetbrains-plugins.lib."${system}".buildIdeWithPlugins pkgs.jetbrains;
 in {
   # TODO: flesh out more!!
-  home.packages = with nix-jetbrains-plugins.lib."${system}"; [
+  home.packages = [
     # Adds the latest IDEA version with the latest compatible version of "com.intellij.plugins.watcher".
-    (buildIdeWithPlugins pkgs.jetbrains "idea-ultimate" ["com.intellij.plugins.watcher"])
-    (buildIdeWithPlugins pkgs.jetbrains "clion" ["com.intellij.plugins.watcher"])
-    (buildIdeWithPlugins pkgs.jetbrains "rust-rover" ["com.intellij.plugins.watcher"])
-    (buildIdeWithPlugins pkgs.jetbrains "pycharm-professional" ["com.intellij.plugins.watcher"])
+    (mkJetbrains "idea" ["com.intellij.plugins.watcher"])
+    (mkJetbrains "clion" ["com.intellij.plugins.watcher"])
+    (mkJetbrains "rust-rover" ["com.intellij.plugins.watcher"])
+    (mkJetbrains "pycharm" ["com.intellij.plugins.watcher"])
   ];
 }
